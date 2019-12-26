591231408dab79d056cb9407d9317aec2f8eb725
apokalipsa44 <m_lachowicz@wp.pl>
2019-12-26 14:13:06 +0100

    first commit

---
+++ src/main/java/com/mchau/oodo/OodoApplication.java
@@ -0,0 +1,13 @@
+package com.mchau.oodo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OodoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OodoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mchau/oodo/OodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mchau.oodo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OodoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
