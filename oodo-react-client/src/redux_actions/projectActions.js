dce7e2c2baca94cacbffcd4328d66d248e34bacf
apokalipsa44 <m_lachowicz@wp.pl>
2020-01-07 00:56:36 +0100

    go to sleep

--- oodo-react-client/src/redux_actions/projectActions.js
+++ oodo-react-client/src/redux_actions/projectActions.js
@@ -7,9 +7,11 @@ export const createProject = (project, history) => async dispach => {
     try {
         const resp = await axios.post("http://localhost:8080/api/project", project)
         history.push("/dashboard")
-        // console.log(history)
+        dispach({
+            type: GET_ERRORS,
+            payload: {}
+        })
     } catch (error) {
-        console.log(error.response.data)
         dispach({
             type: GET_ERRORS,
             payload: error.response.data
