56c98d37be9b02947fa6bc3730c69b5a8584b5c3
apokalipsa44 <m_lachowicz@wp.pl>
2020-01-16 23:35:23 +0100

    project task reducer done

--- oodo-react-client/src/redux_reducers/index.js
+++ oodo-react-client/src/redux_reducers/index.js
@@ -1,10 +1,12 @@
 import {combineReducers} from "redux";
 import errorReducer from "./errorReducer";
 import projectReducer from "./projectReducer";
+import projectTaskReducer from "./projectTaskReducer";
 
 export default combineReducers({
     errors: errorReducer,
-    project: projectReducer
+    project: projectReducer,
+    backlog:projectTaskReducer
 
 })
 
---
+++ oodo-react-client/src/redux_reducers/projectTaskReducer.js
@@ -0,0 +1,29 @@
+import {DELETE_PROJECT_TASK, GET_PROJECT_TASK, GET_BACKLOG} from "../redux_actions/types";
+
+const initialState = {
+    projectTasks: [],
+    projectTask: {}
+}
+
+export default function (state = initialState, action) {
+    switch (action.type) {
+        case GET_BACKLOG:
+            return {
+                ...state,
+                projectTasks: action.payload
+            }
+        case GET_PROJECT_TASK:
+            return {
+                ...state,
+                projectTask: action.payload
+            }
+        case DELETE_PROJECT_TASK:
+            return {
+                ...state,
+                projectTask:action.payload
+            }
+        default:
+            return state;
+    }
+
+}
