376e3a63a859a0ff3d7a4e4a64d4ab2fc48b4d2f
apokalipsa44 <m_lachowicz@wp.pl>
2020-01-23 23:29:42 +0100

    user details implemented

--- oodo-spring-api/src/main/java/com/mchau/oodo/model/User.java
+++ oodo-spring-api/src/main/java/com/mchau/oodo/model/User.java
@@ -3,17 +3,20 @@ package com.mchau.oodo.model;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.*;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
+import java.util.Collection;
 import java.util.Date;
 
 @Entity
 @Getter
 @Setter
 @NoArgsConstructor
-public class User {
+public class User  extends UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -41,5 +44,30 @@ public class User {
     @PreUpdate
     void onUpdate() {
         this.updatedAt = new Date();
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
     }
 }
