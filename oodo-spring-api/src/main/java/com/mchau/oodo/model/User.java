13552efe31acfb1aacb8b76ea9e0c397f22d10aa
apokalipsa44 <m_lachowicz@wp.pl>
2020-01-23 23:18:14 +0100

    #81 user class added

--- oodo-spring-api/src/main/java/com/mchau/oodo/model/User.java
+++ oodo-spring-api/src/main/java/com/mchau/oodo/model/User.java
@@ -1,4 +1,45 @@
 package com.mchau.oodo.model;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import java.util.Date;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Email(message = "Your username is Your Email")
+    @NotBlank(message = "username must be provided")
+    @Column(unique = true)
+    private String username;
+
+    @NotBlank(message = "password must be provided")
+    private String password;
+
+    @Transient
+    private String confirmPassword;
+
+    private Date createdAt;
+
+    private Date updatedAt;
+
+    @PrePersist
+    void onCreate() {
+        this.createdAt = new Date();
+    }
+
+    @PreUpdate
+    void onUpdate() {
+        this.updatedAt = new Date();
+    }
 }
