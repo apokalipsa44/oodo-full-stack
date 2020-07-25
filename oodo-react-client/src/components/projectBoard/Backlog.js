cbc147beb6ac55f63e82eab03767cf08e138b720
apokalipsa44 <m_lachowicz@wp.pl>
2020-07-25 14:08:36 +0200

    state from props done

--- oodo-react-client/src/components/projectBoard/Backlog.js
+++ oodo-react-client/src/components/projectBoard/Backlog.js
@@ -2,12 +2,56 @@ import React, { Component } from "react";
 import ProjectTask from "./projectTask/ProjectTask";
 import { DragDropContext, Droppable } from "react-beautiful-dnd";
 
+let todoItems = [];
+let inProgressItems = [];
+let doneItems = [];
+
 class Backlog extends Component {
-  state = {
-    todoItems : [],
-    inProgressItems:[],
-    doneItems: []
-  };
+  constructor(props) {
+    super(props);
+    this.state = {
+      todoItems: ["ania", "basia"],
+      inProgressItems: [],
+      doneItems: [],
+    };
+  }
+
+  static getDerivedStateFromProps(props, state) {
+    let tasks = props;
+    todoItems = [];
+    inProgressItems = [];
+    doneItems = [];
+
+    // tasks.project_tasks_prop.map((project_task, index) => {
+    //   return (
+    //     <ProjectTask
+    //       key={project_task.id}
+    //       project_task={project_task}
+    //       index={index}
+    //     />
+    //   );
+    // });
+    for (let i = 0; i < tasks.project_tasks_prop.length; i++) {
+      if (tasks.project_tasks_prop[i].status === "TO_DO") {
+        todoItems.push(tasks.project_tasks_prop[i]);
+        console.log(todoItems);
+      }
+
+      if (tasks.project_tasks_prop[i].status === "IN_PROGRESS") {
+        inProgressItems.push(tasks.project_tasks_prop[i]);
+      }
+
+      if (tasks.project_tasks_prop[i].status === "DONE") {
+        doneItems.push(tasks.project_tasks_prop[i]);
+      }
+    }
+    return {
+      todoItems: todoItems,
+      inProgressItems: inProgressItems,
+      doneItems: doneItems,
+    };
+  }
+
   onDragEnd = (result) => {
     const { destination, source, draggableId } = result;
     if (!destination) {
@@ -20,53 +64,36 @@ class Backlog extends Component {
       return;
     }
 
-    console.log('====================================');
+    console.log("====================================");
     console.log(this.state);
-    console.log('====================================');
-    
-    const column = this.state.columns[source.droppableId];
-    const newTaskIds = Array.from(column.tasksIDs);
-    newTaskIds.splice(source.index, 1);
-    newTaskIds.splice(destination.index, 0, draggableId);
+    console.log("====================================");
 
-    
-
-    
-
-
-
-    this.setState(newState);
+    // newTaskIds.splice(source.index, 1);
+    // newTaskIds.splice(destination.index, 0, draggableId);
   };
   render() {
     const { project_tasks_prop } = this.props;
+    let columns=this.state;
+    console.log(this.state);
+    let todoItems = columns.todoItems;
+    let inProgressItems = columns.inProgressItems;
+    let doneItems =columns.doneItems;
+    console.log(todoItems);
 
-    const tasks = project_tasks_prop.map((project_task, index) => {
-      return (
-        <ProjectTask
-          key={project_task.id}
-          project_task={project_task}
-          index={index}
-        />
-      );
+    todoItems.map(item=>{
+      return ({
+       
+      });
     });
-
-    let todoItems = [];
-    let inProgressItems = [];
-    let doneItems = [];
-
-    for (let i = 0; i < tasks.length; i++) {
-      if (tasks[i].props.project_task.status === "TO_DO") {
-        todoItems.push(tasks[i]);
-      }
-
-      if (tasks[i].props.project_task.status === "IN_PROGRESS") {
-        inProgressItems.push(tasks[i]);
-      }
-
-      if (tasks[i].props.project_task.status === "DONE") {
-        doneItems.push(tasks[i]);
-      }
-    }
+    // const tasks = project_tasks_prop.map((project_task, index) => {
+    //   return (
+    //     <ProjectTask
+    //       key={project_task.id}
+    //       project_task={project_task}
+    //       index={index}
+    //     />
+    //   );
+    // });
 
     return (
       <div className="container">
